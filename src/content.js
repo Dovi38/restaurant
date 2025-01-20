3cc15104535902f8d691986607eebc1e5894ec39
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-01-20 20:47:56 +0000

    onload function

--- src/content.js
+++ src/content.js
@@ -1,1 +1,20 @@
-export const content = "Hello js";
+import "./style.css";
+const mainPage = document.querySelector("#content");
+export const contents = "Hello js";
+//create landing page here. export function of loading to index.js
+// const pageContent = document.querySelector("#content");
+// console.log(pageContent);
+const createHeadline = () => {
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  const title = document.createElement("h1");
+  title.classList.add("text");
+  title.textContent = "Fresh made for everyone's taste";
+  headline.appendChild(title);
+  mainPage.appendChild(headline);
+  return headline;
+};
+
+export const loadContent = () => {
+  createHeadline();
+};
