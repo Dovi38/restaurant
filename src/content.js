6e466fa02947c7ba26eb43ae3622bd721ca03aa3
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-01-22 21:16:46 +0000

    Write setImg function

--- src/content.js
+++ src/content.js
@@ -1,6 +1,14 @@
 import "./style.css";
+import plate from "./assets/plate";
 const mainPage = document.querySelector("#content");
+
 export const contents = "Hello js";
+export const setImg = () => {
+  mainPage.style.backgroundImage = plate;
+  mainPage.style.backgroundSize = "cover";
+  mainPage.style.backgroundPosition = "center";
+};
+
 //create landing page here. export function of loading to index.js
 // const pageContent = document.querySelector("#content");
 // console.log(pageContent);
