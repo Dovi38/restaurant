a0a9241f1d9d1becff6e152726dc4ba9bf14b614
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-01-23 22:04:48 +0000

    Image uploading

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import "./style.css";
+import "./styles.css";
 
 import { contents, loadContent, setImg } from "./content.js";
 
 console.log(contents);
-
+document.addEventListener("DOMContentLoaded", setImg);
 window.addEventListener("load", loadContent);
-setImg();
