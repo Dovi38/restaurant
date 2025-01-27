92bd555df4fd095a8839f9c841ac168e93f5bbfc
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-01-27 22:03:34 +0000

    Create container for image. Add h1,h2 to headline.

--- src/content.js
+++ src/content.js
@@ -1,14 +1,16 @@
 import "./styles.css";
-import Plate from "./assets/plate.jpg";
+import paella from "./assets/paella.jpg";
 const mainPage = document.querySelector("#content");
 
 export const contents = "Hello js";
-export const setImg = () => {
-  console.log(mainPage);
-  const myImage = new Image();
-  myImage.src = Plate;
+export const imageDiv = () => {
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("imageDiv");
+  const myImage = document.createElement("img");
+  myImage.src = paella;
   myImage.classList.add("plate");
-  mainPage.appendChild(myImage);
+  imageContainer.appendChild(myImage);
+  mainPage.appendChild(imageContainer);
   return mainPage;
 };
 
@@ -18,9 +20,12 @@ export const setImg = () => {
 const createHeadline = () => {
   const headline = document.createElement("div");
   headline.classList.add("headline");
-  const title = document.createElement("h1");
-  title.classList.add("text");
+  const name = document.createElement("h1");
+  name.classList.add("text");
+  name.textContent = "Marion Restaurant";
+  const title = document.createElement("h2");
   title.textContent = "Fresh made for everyone's taste";
+  headline.appendChild(name);
   headline.appendChild(title);
   mainPage.appendChild(headline);
   return headline;
