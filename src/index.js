c8acba4b748636611394c13e50a8573a681559a8
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-01-29 21:03:07 +0000

    Add menu button functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import "./styles.css";
 
 import { loadHomePage } from "./home.js";
+import { loadMenuPage } from "./menu.js";
 
 //loads home page on initial page load
 window.addEventListener("load", loadHomePage);
 // const content = document.querySelector("#content");
 const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
 
 //loads home page on home button click
 homeBtn.addEventListener("click", () => {
   loadHomePage();
+});
+menuBtn.addEventListener("click", () => {
+  loadMenuPage();
 });
