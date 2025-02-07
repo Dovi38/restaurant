596946c8aefcd4ef671d70bda32d7b89b4695428
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-07 21:40:05 +0000

    Add btn about

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { loadAboutPage } from "./about.js";
 
 //loads home page on initial page load
 window.addEventListener("load", loadHomePage);
-// const content = document.querySelector("#content");
+
 const homeBtn = document.querySelector("#home");
 const menuBtn = document.querySelector("#menu");
 const aboutBtn = document.querySelector("#about");
