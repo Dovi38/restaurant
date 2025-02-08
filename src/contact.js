94e989f063854d47974d2db59d1aaf5ba6f11e44
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-08 18:47:51 +0000

    add two elements to page

--- src/contact.js
+++ src/contact.js
@@ -5,6 +5,9 @@ import { mainPage } from "./home";
 const createContacts = () => {
   const mainContainer = document.createElement("div");
   mainContainer.classList.add("contact");
+  const createHeader = document.createElement("h1");
+  createHeader.textContent = "Contact Us";
+  mainContainer.appendChild(createHeader);
   mainPage.appendChild(mainContainer);
 };
 
