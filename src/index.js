873cb3eef73594a533d337ef7722e7df4fa44428
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-08 18:31:55 +0000

    Add contact btn functionality

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { loadHomePage } from "./home.js";
 import { loadMenuPage } from "./menu.js";
 import { loadAboutPage } from "./about.js";
-import { loadContactPage } from "./contacs.js";
+import { loadContactPage } from "./contact.js";
 
 //loads home page on initial page load
 window.addEventListener("load", loadHomePage);
@@ -11,6 +11,7 @@ window.addEventListener("load", loadHomePage);
 const homeBtn = document.querySelector("#home");
 const menuBtn = document.querySelector("#menu");
 const aboutBtn = document.querySelector("#about");
+const contactBtn = document.querySelector("#contact");
 
 //loads home page on home button click
 homeBtn.addEventListener("click", () => {
@@ -21,4 +22,7 @@ menuBtn.addEventListener("click", () => {
 });
 aboutBtn.addEventListener("click", () => {
   loadAboutPage();
+});
+contactBtn.addEventListener("click", () => {
+  loadContactPage();
 });
