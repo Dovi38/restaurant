ad583c2361e1ba7d25b7c76fd94d8d92b43cb61a
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-08 18:03:09 +0000

    Add class to img container

--- src/about.js
+++ src/about.js
@@ -1,19 +1,20 @@
 import "./styles.css";
-import biscuit from "./assets/biscuit.jpg";
+import plate from "./assets/res6.jpg";
 import { mainPage } from "./home";
 
 const aboutSection = () => {
-  const textContainer = document.createElement("div");
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("image-text");
   const imgContainer = document.createElement("img");
   imgContainer.classList.add("imgRestaurant");
-  imgContainer.src = biscuit;
-  textContainer.appendChild(imgContainer);
+  imgContainer.src = plate;
+  contentContainer.appendChild(imgContainer);
   const descriptionText = document.createElement("p");
   descriptionText.classList.add("description");
   descriptionText.textContent =
-    "At The Garden Table, we're more than just a restaurant; we're a community hub.  We strive to create a warm and inviting atmosphere where friends and families can gather to enjoy delicious food and connect with the source of their meals.  Our commitment to our garden, as we prioritize eco-friendly practices in all aspects of our operations.  Come experience the true taste of our region at The Garden Table, where every bite is a celebration of fresh, local flavors.";
-  textContainer.appendChild(descriptionText);
-  mainPage.appendChild(textContainer);
+    "At The Marion restaurant, we're more than just a restaurant; we're a community hub.  We strive to create a warm and inviting atmosphere where friends and families can gather to enjoy delicious food and connect with the source of their meals.  Our commitment to our garden, as we prioritize eco-friendly practices in all aspects of our operations.  Come experience the true taste of our region at The Marion restaurant, where every bite is a celebration of fresh, local flavors.";
+  contentContainer.appendChild(descriptionText);
+  mainPage.appendChild(contentContainer);
 };
 
 export const loadAboutPage = () => {
