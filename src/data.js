42ac87cdb068bdd862445b4891e8cb1600a88149
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-10 19:09:58 +0000

    Create data file

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const menuData = [
+  {
+    dishName: "Fresh food platter 1",
+    content:
+      "Fresh olives, cherry tomatoes, cucumber, feta cheese, and mozzarella.",
+    platterPrice: "£ 10",
+  },
+  {
+    dishName: "Fresh food platter 2",
+    content:
+      "Tzatziki, dolmas, fresh hummus, sliced cucumbers, and grilled halloumi.",
+    platterPrice: "£ 15",
+  },
+  {
+    dishName: "Fresh food platter 3",
+    content:
+      "Grilled calamari, fresh shrimp, and smoked salmon with a lemon dill sauce.",
+    platterPrice: "£ 25",
+  },
+  {
+    dishName: "Fresh food platter 4",
+    content: "Assorted cheeses, crackers, and fresh grapes.",
+    platterPrice: "£ 12",
+  },
+  {
+    dishName: "Fresh food platter 5",
+    content: "A vibrant mix of heirloom tomatoes, cucumber, and bell peppers.",
+    platterPrice: "£ 9",
+  },
+];
+
+export const contactData = [
+  { header: "Call Us:", information: "+4478 64837007, +4467 75758390" },
+  { header: "Location:", information: "56 Walnuts Street, London, SP3 9GH" },
+  { header: "Working Hours:", information: "Mon-Sun 10am-9pm" },
+];
