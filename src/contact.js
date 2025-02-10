d2d6c3a1a480cec10cff1336a7d9c1cbb5cdde79
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2025-02-10 19:13:02 +0000

    Add loop to create same elements

--- src/contact.js
+++ src/contact.js
@@ -1,45 +1,37 @@
 import "./styles.css";
-
 import { mainPage } from "./home";
+import { contactData } from "./data";
 
-const createContacts = () => {
+const createContacts = (infoData) => {
   const mainContainer = document.createElement("div");
   const modalContact = document.createElement("div");
   const createHeader = document.createElement("h1");
-  const subHeader = document.createElement("h3");
-  const subHeader2 = document.createElement("h3");
-  const subHeader3 = document.createElement("h3");
-  const phoneNumber = document.createElement("p");
-  const location = document.createElement("p");
-  const workingHours = document.createElement("p");
 
   mainContainer.classList.add("contact");
   modalContact.classList.add("modal-container");
   createHeader.classList.add("contactUs");
 
   createHeader.textContent = "Contact Us";
-  subHeader.textContent = "Call us:";
-  phoneNumber.textContent = "+4478 64837007, +4467 75758390";
-  subHeader2.textContent = "Location:";
-  location.textContent = " 56 Walnuts Street, London, SP3 9GH";
-  subHeader3.textContent = "Working-Hours:";
-  workingHours.textContent = "Mon-Sun 10am-9pm";
-
   modalContact.appendChild(createHeader);
-  modalContact.appendChild(subHeader);
-  modalContact.appendChild(phoneNumber);
 
-  modalContact.appendChild(subHeader2);
-  modalContact.appendChild(location);
+  infoData.forEach((elem) => {
+    const subHeader = document.createElement("h2");
+    subHeader.textContent = elem.header;
 
-  modalContact.appendChild(subHeader3);
-  modalContact.appendChild(workingHours);
+    const textInfo = document.createElement("p");
+    textInfo.textContent = elem.information;
+
+    modalContact.appendChild(subHeader);
+    modalContact.appendChild(textInfo);
+  });
 
   mainContainer.appendChild(modalContact);
   mainPage.appendChild(mainContainer);
+
+  return mainPage;
 };
 
 export const loadContactPage = () => {
   mainPage.innerHTML = "";
-  createContacts();
+  createContacts(contactData);
 };
